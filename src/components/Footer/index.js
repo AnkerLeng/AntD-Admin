54234e04c35adb8cdcb187299142bda63649329d
anker <553628212@qq.com>
2019-07-17 11:33:56 +0800

    第3章 主页面架构设计

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.less'
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <div className="footer">
+               版权所有：冰箱（推荐使用谷歌浏览器，可以获得更佳操作页面体验） 技术支持：冰箱
+            </div>
+        );
+    }
+}
